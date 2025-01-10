d224d4b4617343b5942c904fada6fd7e3f1bef74
Martyna Witkowska <mar.witkowska@gmail.com>
2025-01-10 10:10:12 +0100

    model serializers

---
+++ api/users/serializers/organization_serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from users.models import Organization
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = ['id', 'name', 'tenant', 'created_at', 'updated_at']
+
